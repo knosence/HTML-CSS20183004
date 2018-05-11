87ba145eedad061cc736b9295c720d0e5a1fae95
knosence <nadario.seays@gmail.com>
2018-05-11 12:34:06 -0400

    Add Somthing

--- myFirstExpressSite/routes/index.js
+++ myFirstExpressSite/routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    const atlTeams = [
+        `Braves`,
+        `Falcants`,
+        `United`,
+        `Hawks`
+    ]
+    res.render('index', {atlTeams});
 });
+
+router.get('/football', (req,res)=>{
+    const atlTeams = [
+        `Falcan'ts`,
+        `Braves`,
+        `United`,
+        `Hawks`
+    ]
+    res.render('index', {atlTeams});
+})
 
 module.exports = router;
